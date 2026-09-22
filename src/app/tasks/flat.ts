4de531bb16f33ec1f2f667d0d43ef7db1e994368
Artemii Khafizov <50587860+Owleek@users.noreply.github.com>
2026-09-22 15:25:02 +0300

    task: pp-3.2(stack)

--- src/app/tasks/flat.ts
+++ src/app/tasks/flat.ts
@@ -1,19 +1,47 @@
 // 1. Рекурсия
+const sourceArray = [[[1, [2]], [3, 4], [[5, [6]]], 7, 8]]
 
-const flatR = <T extends unknown[]>(arr: T): T  => {
-   const result: unknown[] = []
+type DeepArr<T> = Array<T | DeepArr<T>>
 
-   const innerFlat = (element: unknown) => {
-        if (!Array.isArray(element)) {
-            result.push(element)
-        } else element.forEach(el => innerFlat(el))
+const flatR = <T>(arr: DeepArr<T>): T[]  => {
+   const result: T[] = []
+
+   const innerFlat = (element: T | DeepArr<T>) => {
+        if (Array.isArray(element)) {
+            element.forEach(el => innerFlat(el))
+        } else result.push(element)
    }
 
    arr.forEach(element => innerFlat(element))
    
-   return result as T
+   return result
 };
 
-console.log(flatR([1, 2, [3, 4], [[5, [6]]], 7, 8])); // [1,2,3,4,5,6,7,8]
+console.log(flatR(sourceArray)); // [1,2,3,4,5,6,7,8]
 
-// 2. Стек / Очередь
+// 2. Стек 
+
+const flat = <T,>(arr: DeepArr<T>): T[] => {
+    const result: T[] = []
+    const stack: DeepArr<T> = []
+
+    // В Данном случае мы проходим по каждому элементу и наполняем стек слева на право
+    // Тут нет никакой лишней операции
+    const reverseAndPushToStack = (array: DeepArr<T>) => {
+      for (let i = arr.length - 1; i >= 0; i--) {
+        stack.push(arr[i])
+      }
+    }
+
+    reverseAndPushToStack(arr)
+
+    while(stack.length) {
+      const el = stack.pop()!
+      if (Array.isArray(el)) reverseAndPushToStack(arr)
+      else result.push(el)
+    }
+
+    return result
+};
+
+console.log(flat(sourceArray)); // [1,2,3,4,5,6,7,8]
